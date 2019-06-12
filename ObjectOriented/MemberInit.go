b3f5a7d553687f15dab8f26adff59e7835e2beaa
dongwenbin <dwbdeveloper1991@163.com>
2019-06-12 23:36:49 +0800

    面向对象

--- ObjectOriented/MemberInit.go
+++ ObjectOriented/MemberInit.go
@@ -1,6 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"math/big"
+)
+
+type ClassA struct {
+	id       int
+	name     string
+	isCan    bool
+	p        uintptr
+	money    big.Float
+	score    float64
+	array    [10]int
+	classB   ClassB
+	objArray [10]ClassB
+	slice    []int
+}
+
+type ClassB struct {
+	a string
+}
+
 // 类成员初始化总结
 func main() {
+	var c1 ClassA
+	fmt.Println(c1.objArray)
 
+	c2 := ClassA{}
+	fmt.Println(c2)
 }
