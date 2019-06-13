0218e1f6e315f382f14ead2736da07b3396b80f4
WebbDong <webbdong@swell.link>
2019-06-14 02:20:00 +0800

    文件解压缩案例

--- FileCompress/src/Main.go
+++ FileCompress/src/Main.go
@@ -1,6 +1,101 @@
 package main
 
-// 文件压缩解压案例
-func main() {
+import (
+	"fmt"
+	. "github.com/lxn/walk/declarative"
+)
 
+type Foo struct {
+	Bar string
+	Baz int
+}
+
+type MyWindow struct {
+	MainWindow
+}
+
+func main() {
+	foo := &Foo{"b", 0}
+
+	MyWindow{
+		MainWindow{
+			Title:   "Walk RadioButton Example",
+			MinSize: Size{320, 240},
+			Layout:  VBox{},
+			DataBinder: DataBinder{
+				DataSource: foo,
+				AutoSubmit: true,
+				OnSubmitted: func() {
+					fmt.Println(foo)
+				},
+			},
+			Children: []Widget{
+				// RadioButtonGroup is needed for data binding only.
+				RadioButtonGroup{
+					DataMember: "Bar",
+					Buttons: []RadioButton{
+						RadioButton{
+							Name:  "aRB",
+							Text:  "A",
+							Value: "a",
+						},
+						RadioButton{
+							Name:  "bRB",
+							Text:  "B",
+							Value: "b",
+						},
+						RadioButton{
+							Name:  "cRB",
+							Text:  "C",
+							Value: "c",
+						},
+					},
+				},
+				Label{
+					Text:    "A",
+					Enabled: Bind("aRB.Checked"),
+				},
+				Label{
+					Text:    "B",
+					Enabled: Bind("bRB.Checked"),
+				},
+				Label{
+					Text:    "C",
+					Enabled: Bind("cRB.Checked"),
+				},
+				RadioButtonGroup{
+					DataMember: "Baz",
+					Buttons: []RadioButton{
+						RadioButton{
+							Name:  "oneRB",
+							Text:  "1",
+							Value: 1,
+						},
+						RadioButton{
+							Name:  "twoRB",
+							Text:  "2",
+							Value: 2,
+						},
+						RadioButton{
+							Name:  "threeRB",
+							Text:  "3",
+							Value: 3,
+						},
+					},
+				},
+				Label{
+					Text:    "1",
+					Enabled: Bind("oneRB.Checked"),
+				},
+				Label{
+					Text:    "2",
+					Enabled: Bind("twoRB.Checked"),
+				},
+				Label{
+					Text:    "3",
+					Enabled: Bind("threeRB.Checked"),
+				},
+			},
+		},
+	}.Run()
 }
