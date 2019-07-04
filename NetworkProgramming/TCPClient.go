b591b727f9164bf91ad7cfce13aefb60d8807acd
dongwenbin <dwbdeveloper1991@163.com>
2019-07-05 01:06:22 +0800

    网络编程

--- NetworkProgramming/TCPClient.go
+++ NetworkProgramming/TCPClient.go
@@ -13,4 +13,13 @@ func main() {
 	}
 	defer conn.Close()
 
+	conn.Write([]byte("你好吗？"))
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("读取到的数据：", string(buf[:n]))
 }
