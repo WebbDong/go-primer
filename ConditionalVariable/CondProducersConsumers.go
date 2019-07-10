eb6952b50f1282efe8f59d395992aedbf4e4a6a7
WebbDong <webbdong@swell.link>
2019-07-10 11:02:41 +0800

    条件变量，生产者消费者模型

--- ConditionalVariable/CondProducersConsumers.go
+++ ConditionalVariable/CondProducersConsumers.go
@@ -9,6 +9,29 @@ import (
 
 /*
 	条件变量实现生产者消费者模型
+
+	条件变量，经常与锁结合使用
+
+	条件变量结构体：
+		type Cond struct {
+			noCopy noCopy
+
+			// L is held while observing or changing the condition
+			L Locker
+
+			notify  notifyList
+			checker copyChecker
+		}
+
+	条件变量的操作函数：
+		Wait方法：
+			1、阻塞
+			2、释放自己拥有的互斥锁(cond.L.Unlock())，这2步为一个原子操作
+			3、当被唤醒后，Wait方法返回时，解除阻塞并重新获取互斥锁(cond.L.Lock())
+		Signal方法：
+			给一个阻塞在该条件变量上的Go程发送唤醒通知
+		Broadcast方法：
+			给阻塞在该条件变量上的所有Go程发送唤醒通知
 */
 func main() {
 	// 条件变量实现生产者消费者模型
