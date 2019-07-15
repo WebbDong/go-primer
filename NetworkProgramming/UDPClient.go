c28b73cc1590c0b6e2336a8e48a9d800605ed073
dongwenbin <dwbdeveloper1991@163.com>
2019-07-15 13:49:14 +0800

    UDP

--- NetworkProgramming/UDPClient.go
+++ NetworkProgramming/UDPClient.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+)
 
+func main() {
+	conn, err := net.Dial("udp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	conn.Write([]byte("Fuck you"))
+
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("读取到的数据：", string(buf[:n]))
 }
--- NetworkProgramming/UDPServer.go
+++ NetworkProgramming/UDPServer.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 // UDP服务端
 func main() {
+	addr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
 
+	buf := make([]byte, 4096)
+	// 返回值：1、读到的字节数，2：客户端的地址，3：error
+	count, udpAddr, err := conn.ReadFromUDP(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("count =", count)
+	fmt.Println("udpAddr =", udpAddr)
+	fmt.Println("读取到的数据：", string(buf))
+
+	conn.WriteToUDP([]byte("Hello World"), udpAddr)
 }
